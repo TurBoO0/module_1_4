f3b8665fb7cc243053ee43d9302e58a78dce4be7
TurBoO0 <127961357+TurBoO0@users.noreply.github.com>
2024-12-22 20:30:59 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Введите ваше имя: ")
+print(f'Количество букв: {len(my_string)}')
+print(f'Ваше имя в ВЕРХНЕМ регистре: {my_string.upper()}')
+print(f'Ваше имя в ВЕРХНЕМ регистре: {my_string.lower()}')
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
